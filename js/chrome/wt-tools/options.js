d93645654c5cdac178df5b96ed7179b2afaf8288
Roland Arsenault <roland@ccom.unh.edu>
2013-12-16 14:12:11 -0500

    Removed the confirm exit prompt for edit pages with unsaved edits. Fixed whitespace

--- js/chrome/wt-tools/options.js
+++ js/chrome/wt-tools/options.js
@@ -1,13 +1,3 @@
 
 $( document ).ready(function(){
-	if(localStorage["confirmExit"]=="true")
-		$("#confirmExit").prop('checked',true);
-	
-	$("#confirmExit").change(function(event){
-		var confirm = $("#confirmExit:checked").val();
-		if(confirm)
-			localStorage["confirmExit"] = true;
-		else
-			localStorage["confirmExit"] = false;
-	});
 });
