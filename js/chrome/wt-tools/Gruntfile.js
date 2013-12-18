7427337aa7246576aa32fa0f226deca4b5220ae3
Roland Arsenault <roland@ccom.unh.edu>
2013-12-17 20:49:56 -0500

    Seperating extension code from build files Added eclipse project files Added Node.js grunt files

---
+++ js/chrome/wt-tools/Gruntfile.js
@@ -0,0 +1,26 @@
+module.exports = function(grunt) {
+
+  // Project configuration.
+  grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
+    jshint: {
+      files: ['Gruntfile.js', 'src/*.js'],
+      options: {
+        // options here to override JSHint defaults
+        globals: {
+          jQuery: true,
+          console: true,
+          module: true,
+          document: true
+        }
+      }
+    }
+  });
+
+  // Load the plugin that provides the "uglify" task.
+  grunt.loadNpmTasks('grunt-contrib-jshint');
+
+  // Default task(s).
+  grunt.registerTask('default', ['jshint']);
+
+};
