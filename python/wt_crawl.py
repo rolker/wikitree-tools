ccf23c50ea712ae8b23223872a3705818b21263c
Roland Arsenault <roland@ccom.unh.edu>
2014-03-21 16:28:30 -0400

    More detailed output, including links to previous step Started adding ability to cache results so a crawl can be resumed

--- python/wt_crawl.py
+++ python/wt_crawl.py
@@ -6,18 +6,23 @@ import wt
 import codecs
 import urllib
 import datetime
+import StringIO
 
 home = None
-gens = 6
+steps = 6
 out = None
+logfn = None
 
 for a in sys.argv[1:]:
     if '=' in a:
         parts = a.split('=',1)
-        if parts[0] == 'gens':
-            gens = int(parts[1])
+        if parts[0] == 'steps':
+            steps = int(parts[1])
         if parts[0] == 'out':
             out = parts[1]
+        if parts[0] == 'log':
+          logfn = parts[1]
+            
     else:
         home = a
 
@@ -25,14 +30,21 @@ c = wt.Connection()
 if home is None:
     home = c.uname
     
-print 'home',home,'gens',gens
+print 'home',home,'steps',steps
 
 data = json.load(c.getPage('action=getPerson&key='+home+'&fields=Id'))
 
+log = None
+if logfn is not None:
+  log = open(logfn('w'))
+
 toCheck = []
 checked = []
+links = {}
+profiles = {}
 
 toCheck.append(data[0]['person']['Id'])
+links[data[0]['person']['Id']] = None
 
 outfile = None
 if out is not None:
@@ -40,15 +52,21 @@ if out is not None:
     outfile.write('<html><head><meta http-equiv="Content-Type" content="text/html; charset=utf-8" /></head><body>\n')
     outfile.write('<h1>'+str(datetime.datetime.now())+'</h1>\n')
 
-for gen in range(gens):
+for step in range(steps+1):
     newToCheck = []
     if outfile is not None:
-        outfile.write('<h1>Degree '+str(gen)+'</h1>\n')
+        outfile.write('<h1>Step '+str(step)+'</h1>\n')
+    count = 1
     for uid in toCheck:
         checked.append(uid)
-        data = json.load(c.getPage('action=getPerson&key='+str(uid)+'&fields=Name,FirstName,LastNameCurrent,Parents,Siblings,Children,Spouses'))[0]['person']
-        if data is not None:
-          print gen,data['Name'],
+        p = c.getPage('action=getPerson&key='+str(uid)+'&fields=Name,FirstName,LastNameCurrent,Parents,Siblings,Children,Spouses')
+        if p is not None:
+          datastr = p.read()
+          if log is not None:
+            log.write(datastr+'\n')
+          data = json.load(StringIO.StringIO(datastr))[0]['person']
+          profiles[uid] = data
+          print step,'steps, ',count,'of',len(toCheck),data['Name'],
           name = ''
           if 'FirstName' in data:
               name += data['FirstName']+' '
@@ -56,30 +74,43 @@ for gen in range(gens):
           if 'LastNameCurrent' in data:
               name += data['LastNameCurrent']+' '
               print data['LastNameCurrent'],
-          print
+          if links[uid] is not None:
+            print '(link:',links[uid][1],profiles[links[uid][0]]['Name'],')'
+          count += 1
           if outfile is not None:
-              outfile.write('<a href="http://www.wikitree.com/wiki/'+data['Name']+'">'+name+'</a><br>\n')
+              outfile.write('Steps: '+str(step)+' <a id="'+data['Name']+'" href="http://www.wikitree.com/wiki/'+data['Name']+'">'+data['Name']+'</a>: '+name)
+              if links[uid] is not None:
+                outfile.write(' (via: '+links[uid][1]+' <a href="#'+profiles[links[uid][0]]['Name']+'">'+profiles[links[uid][0]]['Name']+'</a>)')
+              outfile.write('<br>\n')
               outfile.flush()
           if type(data['Parents']) == type({}):
               for id in data['Parents'].keys():
                   id = int(id)
                   if not (id in toCheck or id in checked or id in newToCheck):
                       newToCheck.append(id)
+                  if not id in links:
+                    links[id] = (uid,'child')
           if 'Siblings' in data and type(data['Siblings']) == type({}):
               for id in data['Siblings'].keys():
                   id = int(id)
                   if not (id in toCheck or id in checked or id in newToCheck):
                       newToCheck.append(id)
+                  if not id in links:
+                    links[id] = (uid,'sibling')
           if 'Children' in data and type(data['Children']) == type({}):
               for id in data['Children'].keys():
                   id = int(id)
                   if not (id in toCheck or id in checked or id in newToCheck):
                       newToCheck.append(id)
+                  if not id in links:
+                    links[id] = (uid,'parent')
           if 'Spouses' in data and type(data['Spouses']) == type({}):
               for id in data['Spouses'].keys():
                   id = int(id)
                   if not (id in toCheck or id in checked or id in newToCheck):
                       newToCheck.append(id)
+                  if not id in links:
+                    links[id] = (uid,'spouse')
           #print checked
           #print toCheck
           #print newToCheck
