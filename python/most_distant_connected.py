20a11d3ac8f8f6a169305489e1d1a59b31082a3c
Roland Arsenault <roland@rolker.net>
2014-12-01 21:32:28 -0500

    Now doing both peternal and maternal, limiting to first N lines for faster debugging

--- python/most_distant_connected.py
+++ python/most_distant_connected.py
@@ -15,6 +15,7 @@ class Profile:
   def __init__(self,row):
     self.uid = int(row[0])
     self.wtid = row[1]
+    self.gender = row[2]
     self.name = row[3]+' '+row[5]
     if row[-3] in ('','0'):
       self.father = None
@@ -46,6 +47,7 @@ def getLine(profile,line='paternal'):
   
 header = None
 
+lcount = 0
 for line in open(sys.argv[1], 'r'):
   row = line.split('\t')
   if header is None:
@@ -56,6 +58,9 @@ for line in open(sys.argv[1], 'r'):
     try:
       p = Profile(row)
       data[p.uid] = p
+      lcount += 1
+      if lcount > 4000000:
+        break
     except (ValueError,ProfileException,IndexError):
       c = min(len(header),len(row))
       for i in range(c):
@@ -65,32 +70,77 @@ for line in open(sys.argv[1], 'r'):
  
 print len(data),'records'
 
+print len(extra_tabs),'profiles with extra tabs',extra_tabs[:10],'...'
+
+
 uids = data.keys()
 uids.sort()
 
-recursives = []
-maxGens = 0
-candidate_lines = []
-
-for uid in uids:
-  try:
-    line = getLine(data[uid])
-  except RuntimeError:
-    recursives.append(data[uid])
-  maxGens = max(maxGens,len(line))
-  if len(line) > 15:
-    candidate_lines.append(line)
-#    print data[uid].name
-#    for p in line:
-#      print '\t',p.wtid,p.name
-
-#for r in recursives:
-#  print 'error:',r
+for atype in ('paternal','maternal'):
+  print atype
   
-print 'max gens:', maxGens
-
-print len(recursives),'profiles with recursion errors'
-
-print len(missing_profiles),'missing profiles',missing_profiles[:10],'...' 
-print len(candidate_lines), 'candidate lines'
-print len(extra_tabs),'profiles with extra tabs',extra_tabs[:10],'...'
+  recursives = []
+  maxGens = 0
+  candidate_lines = []
+  
+  for uid in uids:
+    if data[uid].gender == '':
+      try:
+        line = getLine(data[uid],atype)
+      except RuntimeError:
+        recursives.append(data[uid])
+      maxGens = max(maxGens,len(line))
+      if len(line) > 10:
+        candidate_lines.append(line)
+  #    print data[uid].name
+  #    for p in line:
+  #      print '\t',p.wtid,p.name
+  
+  #for r in recursives:
+  #  print 'error:',r
+    
+  print 'max gens:', maxGens
+  
+  print len(recursives),'profiles with recursion errors'
+  
+  print len(missing_profiles),'missing profiles',missing_profiles[:10],'...' 
+  print len(candidate_lines), 'candidate lines'
+  
+  steps = {}
+  
+  for i in range(len(candidate_lines)):
+    l1 = candidate_lines[i]
+    for j in range(i,len(candidate_lines)):
+      l2 = candidate_lines[j]
+      if l2[0] not in l1 and l1[-1] == l2[-1]:
+        last = 1
+        minlen = min(len(l1),len(l2))
+        try:
+          while l1[-last] == l2[-last] and last < minlen:
+            last += 1
+          s = len(l1)-last+len(l2)-last
+          if s > 5:
+            if not s in steps:
+              steps[s]=[]
+            steps[s].append((l1,l2)) 
+        except IndexError:
+          print l1
+          print l2
+          sys.exit(1)
+        
+  skeys = steps.keys()
+  skeys.sort()
+  
+  print
+  
+  for i in range(1,11):
+    s = skeys[-i]
+    print s
+    for l1,l2 in steps[skeys[-i]]:
+      last = 1
+      minlen = min(len(l1),len(l2))
+      while l1[-last] == l2[-last] and last < minlen:
+        last += 1
+      print '\t',l1[0].wtid,l1[0].name,'\t',l2[0].wtid,l2[0].name,'\tcommon:',l1[-last+1].wtid,l1[-last+1].name
+  
+  
